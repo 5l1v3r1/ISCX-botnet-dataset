f47abe70803d3feda2514deeb1cf68af4f58b297
equiroz <equiroz@ciencias.unam.mx>
2020-03-30 19:58:11 -0600

    Adding extra stats

--- traffic/traffic/__main__.py
+++ traffic/traffic/__main__.py
@@ -5,57 +5,104 @@ from networkx.algorithms import assortativity as assrt
 from networkx.algorithms import efficiency_measures as eff
 from networkx.algorithms import components as comp
 from networkx.algorithms import centrality as ctr
+from networkx.algorithms import shortest_paths as shp
 
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 import numpy as np
 import random as rdm
 
+def plot_hist(values, ax, name='', title='', xlab='', ylab='Frecuencia', nbins=100):
+
+    ax.set_title(title)
+    ax.set_xlabel(xlab)
+    ax.set_ylabel(ylab)
+
+    ax.set_yscale('log')
+    
+    ax.hist(values, bins=np.arange(min(values), max(values), nbins))
+
+    plt.savefig(name + '.png')
+
+
+mal1 = '172.16.253.132'
+mal2 = '192.168.248.165'
+
 conn_df = pd.read_csv(
-    'traffic/res/ISCX_Botnet-Training.csv', 
+    'traffic/res/ISCX_Botnet-Testing.csv',
     header = None,
     error_bad_lines = False,
     names = ['source', 'target']
 )
 
-N = nx.convert_matrix.from_pandas_edgelist(conn_df.sample(frac=0.25))
+conn_weighted_df = conn_df.groupby(
+    ['source', 'target']
+    ).size().reset_index(name='weight')
+
+N = nx.convert_matrix.from_pandas_edgelist(
+    conn_weighted_df,
+    edge_attr='weight'
+    )
 N.name = 'ISCX Botnet'
 
-nx.draw(N, label = 'ISCX Botnet 2014, 25%', with_labels = False, font_size = 5, node_size = 0.5)
-plt.title(N.name)
-#fig = plt.figure()
-#ax = fig.add_subplot(111)
+print(fn.info(N))
+print('Connected components: ', comp.number_connected_components(N))
 
-#h = list(assrt.average_neighbor_degree(N).values())
+n1 = comp.node_connected_component(N, mal1)
+n2 = comp.node_connected_component(N, mal2)
 
-#print(h[:10])
+I = N.subgraph(n1.union(n2))
+I.name = 'Induced Botnet'
+print(fn.info(I))
 
-#ax.set_title('Grado promedio de vecinos')
-#ax.set_yscale('log')
-#ax.set_xlim((-0.1, 0.8))
-#ax.set_xlabel('Grado promedio')
-#ax.set_ylabel('Frecuencia')
-#ax.hist(h, bins=np.arange(min(h), max(h) + 100, 100))
-plt.show()
+#nx.draw(I, label = 'ISCX Botnet 2014, 25%', with_labels = False, font_size = 5, node_size = 0.5)
+#plt.title(N.name)
 
-#print(fn.info(N))
+fig, axs = plt.subplots(1, 1)
 
-#print('Connected components: ', comp.number_connected_components(N))
 
-#print('Clustering coefficient: {0:.4f}'.format(
-#    appr.clustering_coefficient.average_clustering(N)
-#    )
-#)
+h1 = [d for n, d in I .degree()]
+plot_hist(h1, axs, name='cgrad',title='Centralidad de grado',
+ xlab='Fracción del grado máximo')
 
-#print('Connectivity: ', appr.node_connectivity(N))
 
-#print('Assortativity: ', assrt.degree_assortativity_coefficient(N))
+h2 = assrt.average_neighbor_degree(I).values()
+plot_hist(h2, axs, name='cavgnei', title='Grado promedio de vecinos', xlab='Grado promedio')
 
-#print('Avg nei degree: ', assrt.average_neighbor_degree(N))
 
-#print('Local efficiency', eff.local_efficiency(N))
+h3 = ctr.information_centrality(I).values()
+plot_hist(h3, axs, name='cinfo', title='Centralidad de información',
+ xlab='Flujo de información')
 
-#print('Global efficiency', eff.global_efficiency(N))
+h4 = ctr.closeness_centrality(I).values()
+plot_hist(h4, axs, name='cclose', title='Centralidad de cercanía',
+ xlab='Inverso de distancia promedio')
 
-#print('Density: {0:.10f}'.format(fn.density(N)))
+h5 = ctr.betweenness_centrality(I).values()
+plot_hist(h5, axs, name='cbetw', title='Centralidad de intermediario',
+xlab='Fracción de rutas pasantes')
+
+h6 = ctr.eigenvector_centrality(I).values()
+plot_hist(h5, axs, name='ceigen', title='Centralidad de valores propios',
+xlab='Centralidad de los vecinos')
+
+print('Clustering coefficient: {0:.4f}'.format(
+    appr.clustering_coefficient.average_clustering(N)
+    )
+)
+
+print('Average distance:', shp.average_shortest_path_length(I))
+
+print('Connectivity: ', appr.node_connectivity(I))
+
+print('Assortativity: ', assrt.degree_assortativity_coefficient(I))
+
+print('Local efficiency', eff.local_efficiency(I))
+
+print('Global efficiency', eff.global_efficiency(I))
+
+print('Density: {0:.10f}'.format(fn.density(I)))
+
+print('Global reachness centrality:', ctr.global_reaching_centrality(I))
 
